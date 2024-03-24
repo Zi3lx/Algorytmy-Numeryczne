00d2cf35630dc21bbf600c46b88f50ffdbd2bc52
Zi3lx <safurynm@wp.pl>
2024-03-24 23:06:27 +0100

    Fixed commit

--- Projekt2/zad2.cpp
+++ Projekt2/zad2.cpp
@@ -11,6 +11,34 @@ int generateRandomNumber()
     return rand() % 2; // 0-1
 }
 
+vector<double> gauss_seidel(const vector<vector<double>>& A, const vector<double>& b, vector<double> x0, double tol = 1e-6, int max_iter = 1000) {
+    int n = b.size();
+    vector<double> x = x0;
+    for (int iter = 0; iter < max_iter; iter++) 
+    {
+        vector<double> x_new(n, 0.0);
+        for (int i = 0; i < n; i++) {
+            double sum = 0.0;
+            for (int j = 0; j < n; j++) 
+            {
+                if (j != i)
+                    sum += A[i][j] * x_new[j];
+            }
+            x_new[i] = (b[i] - sum) / A[i][i];
+        }
+        double error = 0.0;
+        for (int i = 0; i < n; i++) 
+        {
+            error += abs(x_new[i] - x[i]);
+        }
+        if (error < tol) {
+            return x_new;
+        }
+        x = x_new;
+    }
+    return x;
+}
+
 double monteCarlo(int n, int s, int N) 
 {
     int safeReturns = 0;
